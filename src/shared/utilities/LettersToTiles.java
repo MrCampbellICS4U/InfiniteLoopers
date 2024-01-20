e376206d91cc9b6242735c045feaf31aa560db1d
hoberdidance <339617862@gotvdsb.ca>
2024-01-20 10:06:22 -0500

    Revert "make all tile creation use Tile.getTile"
    This reverts commit 1b0b83951826277bec7a5794875c87aded3eeabd.

--- src/shared/utilities/LettersToTiles.java
+++ src/shared/utilities/LettersToTiles.java
@@ -6,25 +6,34 @@ import game.world.Tiles.*;
 
 public class LettersToTiles {
     // hashmap of letters to tiles objects, not initialized yet
-    public static HashMap<String, String> tilesMap = new HashMap<String, Class>();
+    public static HashMap<String, Class> tilesMap = new HashMap<String, Class>();
 
     public LettersToTiles() {
-        tilesMap.put("N", "NullTile);
-        tilesMap.put("A", "AirTile");
-        tilesMap.put("W", "WaterTile");
-        tilesMap.put("S", "SandTile");
-        tilesMap.put("G", "GrassTile");
-        tilesMap.put("C", "CrateTile");
+        tilesMap.put("N", NullTile.class);
+        tilesMap.put("A", AirTile.class);
+        tilesMap.put("W", WaterTile.class);
+        tilesMap.put("S", SandTile.class);
+        tilesMap.put("G", GrassTile.class);
+        tilesMap.put("C", CrateTile.class);
     }
 
     public static Tile[][][] converToTiles(String[][][] letterMap, int x, int y, int z) {
+        tilesMap.put("N", NullTile.class);
+        tilesMap.put("A", AirTile.class);
+        tilesMap.put("W", WaterTile.class);
+        tilesMap.put("S", SandTile.class);
+        tilesMap.put("G", GrassTile.class);
+        tilesMap.put("C", CrateTile.class);
+
         Tile[][][] tiles = new Tile[letterMap.length][letterMap[0].length][letterMap[0][0].length];
         for (int z1 = 0; z1 < letterMap.length; z1++) {
             for (int y1 = 0; y1 < letterMap[0].length; y1++) {
                 for (int x1 = 0; x1 < letterMap[0][0].length; x1++) {
                     try {
-						tiles[z1][y1][x1] = Tile.getTile(x + x1, y + y1, z + z1, 0, "default", tileMaps.get(letterMap[z1][y1][x1]);
-                        System.out.print(letterMap[z1][y1][x1]);
+                        tiles[z1][y1][x1] = (Tile) tilesMap.get(letterMap[z1][y1][x1])
+                                .getConstructor(int.class, int.class, int.class, int.class, String.class)
+                                .newInstance(x + x1, y + y1, z + z1, 0, "default");
+                        //System.out.print(letterMap[z1][y1][x1]);
                     } catch (Exception e) {
                         System.out.println("Error converting letters to tiles");
                         e.printStackTrace();

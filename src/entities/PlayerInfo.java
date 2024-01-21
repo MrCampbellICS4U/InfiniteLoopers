2d0e42aaa6673e8cb5abd078ea6ec98a431ff939
Brian Zhang <101023105+Be4nis@users.noreply.github.com>
2024-01-21 00:20:00 -0500

    Revert "Merge pull request #35 from MrCampbellICS4U/BAM"
    This reverts commit 4b1cc81537c86b4f4705ed843245c689e521e197, reversing
    changes made to fafad8747c5d904df65743ec7bf4ccc71844002a.

--- src/entities/PlayerInfo.java
+++ src/entities/PlayerInfo.java
@@ -21,20 +21,20 @@ public class PlayerInfo extends EntityInfo {
 	}
 
 	public void customDraw(Graphics g, Client c, int centreRelX, int centreRelY) {
-
-		g.setColor(Color.RED);
-		Graphics2D g2 = (Graphics2D) g;
-		g2.rotate(angle, centreRelX, centreRelY);
-		g.drawImage(c.getImage(), centreRelX-15, centreRelY-15, 80, 40, null);
-		g2.rotate(-angle, centreRelX, centreRelY);
-		//g.drawLine(centreRelX, centreRelY, centreRelX + (int) (Math.cos(angle) * length), centreRelY + (int) (Math.sin(angle) * length));
-
 		if (id == c.getID()) {
 			centreRelX = GlobalConstants.DRAWING_AREA_WIDTH/2;
 			centreRelY = GlobalConstants.DRAWING_AREA_HEIGHT/2;
 			g.setColor(c.getCanvas().getPlayerColor());
 		} else g.setColor(Color.RED);
 		g.fillOval(centreRelX-radius, centreRelY-radius, radius*2, radius*2);
-	}
 
+		// drawing red line for direction
+		int length = 100;
+		g.setColor(Color.RED);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.rotate(angle, centreRelX, centreRelY);
+		g.drawImage(c.getImage(), centreRelX-15, centreRelY-15, 80, 40, null);
+		g2.rotate(-angle, centreRelX, centreRelY);
+		//g.drawLine(centreRelX, centreRelY, centreRelX + (int) (Math.cos(angle) * length), centreRelY + (int) (Math.sin(angle) * length));
+	}
 }

bbe27c2ec379397360cbd1db0f307bab5db12cbf
hoberdidance <154549832+hoberdidance@users.noreply.github.com>
2024-08-17 10:13:56 -0400

    do i remember how to git?

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,23 +1,7 @@
 ;; ethan's emacs config
 
-((nil . ((eval .
-	       (progn
-		 (defun run-client ()
-		   (split-window-vertically)
-		   (other-window 1)
-		   (term "/bin/bash")
-		   (rename-buffer "term-client" t)
-		   (execute-kbd-macro "make client\n"))
-		 (defun run-term ()
-		   (interactive)
-		   (split-window-horizontally)
-		   (other-window 1)
-		   (term "/bin/bash")
-		   (rename-buffer "term-server")
-		   (execute-kbd-macro "make server\n")
-
-		   (run-client)
-		   (run-client)
-		   (other-window -3))
-		 (local-keybind-mode 1)
-		 (evil-define-key 'normal local-keybind-mode-map (kbd "C-;") #'run-term))))))
+(("src" .
+  ((nil . ((eval .
+		 (set-command (progn
+				(async-shell-command "cd ${PWD%/InfiniteLoopers*}/InfiniteLoopers && make server" "server")
+				(async-shell-command "cd ${PWD%/InfiniteLoopers*}/InfiniteLoopers && make client" "client")))))))))
